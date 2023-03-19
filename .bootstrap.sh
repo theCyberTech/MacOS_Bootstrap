9b964e7302fc73108c3bde37471ef73807c68c38
theCyberTech <84775494+theCyberTech@users.noreply.github.com>
2023-03-19 14:53:15 +0800

    Add files via upload

---
+++ .bootstrap.sh
@@ -0,0 +1,38 @@
+#/bin/zsh
+
+# Check if .dotfiles directory exists, back it up, and delete it
+if [ -d "${HOME}/.dotfiles" ]; then
+  echo "Backing up and removing existing .dotfiles directory."
+  mv "${HOME}/.dotfiles" "${HOME}/.dotfiles_backup_$(date +%Y%m%d%H%M%S)"
+fi
+
+# Check if brew is installed and install it if not else update
+if ! command -v brew &> /dev/null
+then
+    echo "Brew not found, Installing."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    # Make sure we’re using the latest Homebrew.
+    echo "Brew already installed, updating."
+    brew update
+fi
+
+# Check if git is installed
+if ! command -v git &> /dev/null
+then
+    echo "git not found, Installing."
+    brew install git
+else
+    echo "git already installed, Updating"
+    brew upgrade git
+fi
+
+# Check if stow is installed
+if ! command -v stow &> /dev/null
+then
+    echo "stow not found, installing."
+    brew install stow
+else
+    echo "stow already installed, updating"
+    brew upgrade stow
+fi
